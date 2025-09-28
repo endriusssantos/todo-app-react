33714b664035923cbbecaf367855fb3b8a08f327
Endrius <endriussilvasantos@gmail.com>
2025-09-28 18:31:42 -0300

    criando componente para o container principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import MainContainer from "./components/MainContainer/MainContainer";
+
 function App() {
   return (
     <>
-      <h1 class="bg-[#3a7bfdff] text-3xl font-bold text-yellow-300 underline">
-        Hello world!
-      </h1>
+      <MainContainer></MainContainer>
     </>
   );
 }
---
+++ src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,20 @@
+import moonIcon from "../../assets/icon-moon.svg";
+
+const MainContainer = () => {
+  return (
+    <>
+      <div className="bg-light-mobile min-h-screen font-[Josefin_Sans]">
+        <div className="flex items-center justify-between px-10 pt-10">
+          <h1 className="text-4xl font-semibold tracking-[0.6rem] text-white">
+            TODO
+          </h1>
+          <button className="cursor-pointer">
+            <img src={moonIcon} alt="Mudar Tema" className="h-8 w-8" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainContainer;
