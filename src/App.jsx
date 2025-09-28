e1e237b9a989ddaf161bfe0cb7e18da226e42108
Endrius <endriussilvasantos@gmail.com>
2025-09-28 17:49:19 -0300

    criando setup inicial do projeto + instalação do Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="bg-[#3a7bfdff] text-3xl font-bold text-yellow-300 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
